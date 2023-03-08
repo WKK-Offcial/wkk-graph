b948620f28d90d0cdc52532e8eecd662e77b6bc5
Paszymaja <36695377+Paszymaja@users.noreply.github.com>
2023-03-08 16:47:52 +0100

    size based on connections

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 from pyvis.network import Network
 import pandas as pd
 import networkx as nx
-from cProfile import label
-
 
 data = pd.read_csv('data.csv',sep=';')
 
-graph = nx.from_pandas_edgelist(data,source='Inviters',target='Name')
+graph = nx.from_pandas_edgelist(data, source='Inviters',target='Name')
 net = Network(notebook=True, height='2000px', width='100%', bgcolor='#222222', font_color='white')
 # net.show_buttons(filter_=['physics'])
 net.from_nx(graph)
@@ -14,27 +12,22 @@ net.from_nx(graph)
 neighbor_map = net.get_adj_list()
 names_list = data['Inviters'].unique()
 
-photos = dict(zip(data.Name,data.photo))
+photos = dict(zip(data.Name, data.photo))
 
 # DISPLAY CONFIG
-for node in net.nodes:        
+for node in net.nodes:
     try:
-        node['value'] = len(neighbor_map[node["id"]])
         node['level'] = len(neighbor_map[node["id"]])
-
         if node['id'] in photos.keys() and type(photos[node['id']]) == str:
             node['shape'] = 'circularImage'
             node['image'] = photos[node['id']]
-
+        node['size'] = 10 + len(neighbor_map[node["id"]])
     except Exception as e:
         print(e)
 
 node = net.get_node('Adam')
-node['color'] = 'yellow'
 node['shape'] = 'circularImage'
 node['image'] = 'images/adam.jpg'
-node['value'] = 90
-node['size'] = 90
 
 # Generate html
 net.show('index.html')
